2cac1990812f20117911b7ff139ec2fbec95423b
simotogn <simonetognocchi3@gmail.com>
2022-09-05 08:55:42 +0200

    modifiche

--- progetto/chess/valuta.py
+++ progetto/chess/valuta.py
@@ -54,31 +54,82 @@ def valuta(board):
     for i in range(8):
         for j in range(8):
             if (board[i][j] != ""):
-                if (board[i][j][0] == "b"):
-                    # valore del pezzo
-                    punteggioBianco += valorePezzi(board[i][j])
-                    # posizione del pezzo 
-                    if(board[i][j][1] == "P"):
-                        punteggioBianco += spostaAvanti[i*8+j]
-                    elif (board[i][j][1] == "C"):
-                        punteggioBianco += alCentro[i*8+j]
-                    elif (board[i][j][1] == "A"):
-                        punteggioBianco += alCentro[i*8+j]  
-                elif (board[i][j][0] == "n"):
+                if (board[i][j][0] == "n"):
                     # valore del pezzo
                     punteggioNero += valorePezzi(board[i][j])
                     # posizione del pezzo 
                     if(board[i][j][1] == "P"):
-                        punteggioNero += spostaAvanti[RuotaScacchiera[i*8+j]]
+                        punteggioNero += spostaAvanti[i*8+j]
                     elif (board[i][j][1] == "C"):
-                        punteggioNero += alCentro[RuotaScacchiera[i*8+j]]
+                        punteggioNero += alCentro[i*8+j]
                     elif (board[i][j][1] == "A"):
-                        punteggioNero += alCentro[RuotaScacchiera[i*8+j]] 
+                        punteggioNero += alCentro[i*8+j]  
+                elif (board[i][j][0] == "b"):
+                    # valore del pezzo
+                    punteggioBianco += valorePezzi(board[i][j])
+                    # posizione del pezzo 
+                    if(board[i][j][1] == "P"):
+                        punteggioBianco += spostaAvanti[RuotaScacchiera[i*8+j]]
+                    elif (board[i][j][1] == "C"):
+                        punteggioBianco += alCentro[RuotaScacchiera[i*8+j]]
+                    elif (board[i][j][1] == "A"):
+                        punteggioBianco += alCentro[RuotaScacchiera[i*8+j]] 
     punteggioBianco += tot_posizioni_valide(board)[0] * 100
     punteggioNero += tot_posizioni_valide(board)[1] * 100
     return (punteggioBianco,punteggioNero)
+
+def stampa_board(board):
+    s = ""
+    print("a b c d e f g h")
+    print()
+    for i in range(8):
+        for j in range(8):
+            if(board[i][j] == ''):
+                s+=". "
+            else:
+                if(board[i][j][0]== "n"):
+                    s+=board[i][j][1].lower() + " "
+                else:
+                    s+=board[i][j][1] + " "
+        s+= "  "
+        s+= str(i+1)
+        s+="\n"
+    print(s)
+
+def main():
+    board = [["" for i in range(8)] for j in range(8)]
+    board[0][0] = "nT"
+    board[0][1] = "nC"
+    board[0][4] = "nR"
+    board[0][5] = "nA"
+    board[1][0] = "nP"
+    board[1][1] = "nP"
+    board[2][5] = "nP"
+    board[2][6] = "nP"
+    board[2][7] = "nT"
+    board[3][3] = "bC"
+    board[2][3] = "nQ"
+    board[3][7] = "nP"
+    board[4][2] = "bP"
+    board[5][0] = "bQ"
+    board[5][1] = "bP"
+    board[6][0] = "bP"
+    board[6][5] = "bP"
+    board[6][6] = "bP"
+    board[6][7] = "bP"
+    board[7][0] = "bT"
+    board[7][4] = "bR"
+    board[7][7] = "bT"
     
+    stampa_board(board)
 
+    print("punt bianco: ", end="")
+    print(valuta(board)[0])
 
+    print("punt nero: ", end="")
+    print(valuta(board)[1])
 
+    print(valuta(board)[0] - valuta(board)[1])
+
+main()
 
